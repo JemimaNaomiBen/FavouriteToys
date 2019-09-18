820c6ab7e5eeabd7053de4d29691051a5c21568e
JemimaNaomiBen <jemmycutiee@gmail.com>
2019-09-18 14:21:25 +0100

    InitCommit TestRun

---
+++ app/src/main/java/com/example/favouritetoys/ToyBox.java
@@ -0,0 +1,54 @@
+package com.example.favouritetoys;
+
+public final class ToyBox {
+
+    public static String[] getToyNames() {
+
+            return new String[] {
+                    "Red Toy Wagon",
+                    "Chemistry Set",
+                    "Yo-Yo",
+                    "Pop-Up Book",
+                    "Generic Uncopyrighted Mouse",
+                    "Finger Paint",
+                    "Sock Monkey",
+                    "Microscope Set",
+                    "Beach Ball",
+                    "BB Gun",
+                    "Green Soldiers",
+                    "Bubbles",
+                    "Spring Toy",
+                    "Fortune Telling Ball",
+                    "Plastic Connecting Blocks",
+                    "Water Balloon",
+                    "Paint-by-Numbers Kit",
+                    "Tuber Head",
+                    "Cool Ball with Holes in It",
+                    "Toy Truck",
+                    "Flying Disc",
+                    "Two-Handed Pogo Stick",
+                    "Toy Hoop",
+                    "Dysmorphia Doll",
+                    "Toy Train",
+                    "Toy Panda",
+                    "Toy Telephone",
+                    "Barrel of Primates",
+                    "Remote Control Car",
+                    "Square Puzzle Cube",
+                    "Football",
+                    "Baby Shakers",
+                    "Baby Horse Dolls",
+                    "Machines that turn into other Machines",
+                    "Teddy Bears",
+                    "Shaved Ice Maker",
+                    "Glow Stick",
+                    "Squirt Guns",
+                    "Yellow Duck",
+                    "Creepy Gremlin Doll",
+                    "Neodymium-Magnet Toy"
+            };
+        }
+    }
+
+
+
